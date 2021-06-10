66f9730dbea04d35708104ba70581f74a657e5d2
Bae Kigun <vanti113@gmail.com>
2021-06-10 19:47:26 +0900

    1.2

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import pandas as pd
 
+daily_df = pd.read_csv("./data/daily_reports.csv")
+totals_df = daily_df[["Confirmed", "Deaths", "Recovered"]
+                     ].sum().reset_index(name="count")
+totals_df = totals_df.rename(columns={"index": "condition"})
