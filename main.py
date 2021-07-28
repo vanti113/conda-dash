27b53760512e38e8b659a27ca2c43931bfb78b57
Bae Kigun <vanti113@gmail.com>
2021-07-28 19:14:28 +0900

    Map is completed

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import plotly.express as px
 from datas import countries_df
 from builders import make_tables
 
-stylesheets = ["https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
-               'https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap']
+# stylesheets = ["https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
+#                'https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap']
 
 style = ["/assets/style.css"]
 app = dash.Dash(__name__, external_stylesheets=style)
 map_figures = px.scatter_geo(countries_df,
+                             title="Confirmed by Country",
                              size_max=40,
                              locations="Country Region",
                              locationmode="country names",
@@ -18,10 +19,13 @@ map_figures = px.scatter_geo(countries_df,
                              hover_data={"Confirmed": ":,2f", "Deaths": ":,2f",
                                          "Recovered": ":,2f", "Country Region": False},
                              color="Confirmed",
+                             color_continuous_scale=px.colors.sequential.Oryel,
                              size="Confirmed",
                              template="plotly_dark",
+                             projection="natural earth",
                              width=800,
-                             height=500
+                             height=500,
+
                              )
 # map_figures.show()
 
@@ -32,21 +36,27 @@ app.layout = html.Div(
                 html.H1(
                     children=["Corona DashBoard"], className="dash-header", style={"fontFamily": "Open Sans, sans-serif"}
                 )],
+            className="header"
         ),
         html.Div(
             children=[
-                html.Div(
-                    children=[
-                        make_tables(countries_df)
-                    ],
-                    className="table-div"
-                ),
-
                 html.Div(
                     [
                         dcc.Graph(id="graph", figure=map_figures)
                     ],
                     className="graph-div"
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children=[
+                                make_tables(countries_df)
+                            ],
+                            className="table-div-div"
+                        )
+
+                    ],
+                    className="table-div"
                 )
             ],
             className="dash-div"
